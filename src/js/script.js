ba66239a7e6a9a4c7cf94c6c9aff9bc6101f2ef0
Mikhail Brazhnik <mikbrazh@gmail.com>
2023-02-22 12:06:05 +0300

    added header bottomline nav

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,8 @@ $(function () {
   $('.header-middleline__hamburger').on( 'click', function() {
     $('.header-middleline__hamburger').toggleClass( 'is-active');
   });
+
+  $('.nav-secondary-item').on( 'click', function() {
+    $($(this).children('ul')).toggleClass('is-active');
+  });
 });
