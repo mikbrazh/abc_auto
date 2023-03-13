eee8dec5d3d1f452cbf729164c35bc3148f0dc11
Mikhail Brazhnik <mikbrazh@gmail.com>
2023-03-13 23:50:47 +0300

    did radio

--- src/js/selection-form__radio.js
+++ src/js/selection-form__radio.js
@@ -1,19 +1,57 @@
 (function () {
 
-  const qwe = document.querySelector('.qwe');
-  const asd1 = document.querySelector('.asd1');
-  const asd2 = document.querySelector('.asd2');
-  const asd3 = document.querySelector('.asd3');
+  // document.querySelectorAll('.selection-form__radio-scale').forEach(function (form) {
+    
+  //   const formRadioScale = document.querySelectorAll('.selection-form__radio-scale');
+  //   const formRadioLabel = form.querySelector('.selection-form__radio-label');
+  
+  //   formRadioLabel.addEventListener('click', function () {
+    //     formRadioScale.classList.toggle('selection-form__radio-scale--color-red');
+    //   });
+    
+    // });
+    
+  
+    
+    document.querySelectorAll('.selection-form__radio-label').forEach(function (lebel) {
+      // const lebelIndex = lebel.indexOf(lebel);
+      
+      lebel.addEventListener('click', function () {
+        console.log(this);
 
-   // Клик по кнопке. Открыть/Закрыть select
-   qwe.addEventListener('click', function () {
-    asd1.classList.toggle('selection-form__radio-scale--color-red');
-    asd2.classList.toggle('selection-form__radio-scale--color-red');
-    asd3.classList.toggle('selection-form__radio-scale--color-red');
+
+        const formScaleAll = document.querySelectorAll('.selection-form__radio-scale');
+
+        
+        const closestFormScale = this.closest('.selection-form__radio-scale');
+        console.log('formScaleAll: ' + formScaleAll);
+        // console.log('closestFormScale: ' + closestFormScale);
+
+        
+        console.log( typeof(formScaleAll) );
+        console.log( formScaleAll[0], formScaleAll[1] );
+        
+        
+        // const closestFormScaleIndex = formScaleAll.indexOf(closestFormScale);
+        // console.log(closestFormScaleIndex);
+
+
+
+        closestFormScale.classList.toggle('selection-form__radio-scale--color-red');
+
+        // console.log(qwe);
+        // const formScale = document.querySelectorAll('.selection-form__radio-scale');
+        // formScaleIndex = formScale.indexOf()
+        // console.log(formScale[0], formScale[1]);
+        // formScale[0].classList.toggle('selection-form__radio-scale--color-red');
+    });
   });
 
 
+    
 
+  // });
 
+  
 
 })();
